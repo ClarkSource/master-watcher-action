5ab41421c9886d44ef8079d17fae4e72eabe6390
Viacheslav Mefodin <mefodin.v@gmail.com>
2019-11-12 19:55:38 +0300

    Brake up circleci checks in slack message

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const web = new WebClient(core.getInput("slack-token"));
 const octokit = new github.GitHub(core.getInput("repo-token"));
 const context = github.context;
 
+const CIRCLECI_SLUG = "circleci-checks";
+
 async function run() {
   console.log("-------------------------------------");
   console.log(context);
   console.log("-------------------------------------");
 
-  const { payload, repo } = context;
+  const { payload } = context;
   const {
     conclusion: status,
     head_commit: commit,
@@ -25,25 +27,56 @@ async function run() {
   const repo_url = payload.repository.html_url;
   const commit_url = `${repo_url}/commit/${commit.id}`;
 
+  slackMessage({
+    icon_emoji: ":red_circle:",
+    text: `*<${commit_url}|${commit_header}>*\n*Build failed on <${repo_url}/commits/master|master branch>.*`,
+    attachments: await circleciAttachments(check_suite_id)
+  });
+}
+
+async function circleciAttachments(check_suite_id) {
   const { data: check_runs } = await octokit.checks.listForSuite({
-    ...repo,
+    ...context.repo,
     check_suite_id
   });
 
-  console.log(check_runs);
+  const circleci_check_run = check_runs.find(check_run => {
+    return (
+      check_run.app.slug === CIRCLECI_SLUG &&
+      check_run.conclusion !== "neutral" &&
+      check_run.conclusion !== "success"
+    );
+  });
 
-  slackMessage(
-    ":red_circle:",
-    `*<${commit_url}|${commit_header}>*\n*Build failed on <${repo_url}/commits/master|master branch>.*\nLinks to Github TBA`
-  );
+  const circleci_regex = /\[(.+)\]\((.+)\)/;
+  return circleci_check_run.output.summary
+    .split("* ")
+    .slice(1)
+    .map(row => {
+      return row.trim().split(" - ");
+    })
+    .filter(check_run => {
+      check_run[1] !== "Success" && check_run[1] !== "Pending";
+    })
+    .map(check_run => {
+      const match = circleci_regex.run(check_run[0]);
+
+      return {
+        fallback: `<${match[2]}|${match[1]}>`,
+        color: "#d30515",
+        title: match[1],
+        title_link: match[2]
+      };
+    });
 }
 
-function slackMessage(icon_emoji, text) {
+function slackMessage({ icon_emoji, text, attachments } = {}) {
   web.chat.postMessage({
     as_user: false,
     channel: core.getInput("slack-channel"),
     icon_emoji,
-    text
+    text,
+    attachments
   });
 }
 
